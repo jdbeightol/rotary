9134ae677a667fce277f199d50bf90c7d1fe6fdd
Jeremy Beightol <jdbeightol@gmail.com>
2014-01-15 18:03:36 -0500

    Initial commit

---
+++ assets/javascript/news.js
@@ -0,0 +1,26 @@
+/*
+ *  Event handlers to control the news menu.
+ */
+
+$('.newslistlink').click(function() {
+  $('#newsframe').attr('src', './' + $(this).attr('id') + '.html');
+  $('.newsletterlink').parent().removeClass('active');
+  $('.newslistlink').parent().removeClass('active');
+  $(this).parent().addClass('active');
+  return false;
+});
+
+$('.newsletterlink').click(function() {
+  $('#newsframe').attr('src', './newsletters/' + $(this).attr('id') + '.html');
+  $('.newslistlink').parent().removeClass('active');
+  $('#newsletters').parent().addClass('active');
+
+  $('.newsletterlink').parent().removeClass('active');
+  $(this).parent().addClass('active');
+  return false;
+});
+
+function setHeight() {
+  $('#newsframe').contents().find('body').css({"min-height": "100", "overflow" : "hidden"});
+  setInterval( "$('#newsframe').height($('#newsframe').contents().find('body').height() + 20)", 1 );
+}
